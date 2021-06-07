1f7360c3173623d527cf15a14d38c8db214bb4bb
HasnainKhanNiazi <hasikhan144@gmail.com>
2021-06-08 00:27:41 +0500

    Updated Code

--- PoleColorCrawler.py
+++ PoleColorCrawler.py
@@ -3,27 +3,10 @@ import numpy as np
 import tqdm
 import os
 
-# Actual
-# lower_green = np.array([25, 52, 72])
-# upper_green = np.array([102, 255, 255])
 
-
-# filename = "C:/Users/HP/Desktop/0a5ed2b8-a13d-467b-9f44-52842a5c77a5.jpg"
-
-# imga=cv2.imread(filename)
-# hsv = cv2.cvtColor(imga, cv2.COLOR_BGR2HSV)
-# greenmask = cv2.inRange(hsv, lower_green, upper_green)
-# rows = greenmask.shape[0]
-# cols = greenmask.shape[1]
-# image = np.zeros((rows, cols, 3), dtype=np.uint8)
-# image[np.where(greenmask == 0)] = (0, 0, 0)
-# image[np.where(greenmask != 0)] = (0, 128, 0)
-# # cv2.imwrite("./{}".format(os.path.basename(filename)), imga)
-# cv2.imwrite("./{}_mask.png".format(os.path.basename(filename)), image)
-
+# This is the final updated code for ColorCrawler which can be used for finding out the color min and max ranges
 
 """
-# NEW
 import cv2
 
 class BoundingBoxWidget(object):
