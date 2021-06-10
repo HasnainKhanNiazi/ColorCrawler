2d17d735d9e3772a51b606020b1552369c5bbc9c
HasnainKhanNiazi <hasikhan144@gmail.com>
2021-06-10 21:48:46 +0500

    Updated Code with RGB color ranges along with pixel values

--- PoleColorCrawlerPixelsLocation.py
+++ PoleColorCrawlerPixelsLocation.py
@@ -9,34 +9,36 @@ def main():
     x = 396
     y = 104
     w = 29
-    h = 364
+    h = 631
 
     # rgb(165,42,42) --> Brown
     # X = 396 + 29 --> 425
     # Y = 104 + 364 --> 468 
-    # location = []
-    # for xL in range(396,425):
-    #     for yL in range(104, 468):
-    #         location.append((xL,yL))
-    newLocation = []
-    for newX in range(396,425):
-        for newY in range(104, 735):
-            newLocation.append((newY, newX))
+    location = []
+    for xL in range(396,(396+29)):
+        for yL in range(104, (104+631)):
+            location.append((yL,xL))
 
-    print(newLocation[-1])
-    # for jj in range(len(newLocation)):
-    #     print(newLocation[jj])
+    Just_Pole = image[y:y+h,x:x+w]
 
-    #     if jj == 100:
-    #         break
-
-    # Just_Pole = image[y:y+h,x:x+w]
-
-    for l in range(0,len(newLocation)):
-        image[newLocation[l]] = [42,42,165]
+    for l in range(0,len(location)):
+        image[location[l]] = [42,42,165]
+    
     cv2.imshow("New Images",image)
-    # cv2.imwrite("newUpdated.jpg",image)
+    cv2.imwrite("newUpdated.jpg",image)
     cv2.waitKey(0)
+    print(len(Just_Pole))
+    locationCounter = 0
+    with open('RGB_Ranges.txt', 'w') as filehandle:
+        filehandle.write("Sequence is RGB")
+        for mm in range(len(Just_Pole)):
+            filehandle.write("\n ************ Row Number " + str(mm) + "************")
+            for nn in range(len(Just_Pole[mm])):
+                filehandle.write("\n RGB value is : " + str(Just_Pole[mm][nn][0]) + "," + str(Just_Pole[mm][nn][0]) + "," + str(Just_Pole[mm][nn][0]) + " ** " + "At X,Y Location " + str(location[locationCounter]))
+                locationCounter += 1
+
+
+
 
 if __name__=="__main__":
     main()
