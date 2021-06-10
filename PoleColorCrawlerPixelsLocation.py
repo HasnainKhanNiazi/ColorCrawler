ee2c09cbfebda9c417998a0bab1b36c8b2ec08b9
HasnainKhanNiazi <hasikhan144@gmail.com>
2021-06-10 15:45:14 +0500

    Final Code for Today

--- PoleColorCrawlerPixelsLocation.py
+++ PoleColorCrawlerPixelsLocation.py
@@ -2,37 +2,41 @@ import cv2
 import numpy as np
 import pandas as pd
 import os
-from PIL import *
-from PIL import Image
 
 def main():
     image = cv2.imread('Updated.jpg', 1)
     image = cv2.resize(image,(800,800))
-
-    imagee = Image.open("Updated.jpg")
-    f = imagee.load()
-
     x = 396
     y = 104
     w = 29
     h = 364
-    points = bbox_dict_to_list(x,y,w,h,(800,800))
-    print(points)
+
+    # rgb(165,42,42) --> Brown
+    # X = 396 + 29 --> 425
+    # Y = 104 + 364 --> 468 
+    # location = []
+    # for xL in range(396,425):
+    #     for yL in range(104, 468):
+    #         location.append((xL,yL))
+    newLocation = []
+    for newX in range(396,425):
+        for newY in range(104, 735):
+            newLocation.append((newY, newX))
+
+    print(newLocation[-1])
+    # for jj in range(len(newLocation)):
+    #     print(newLocation[jj])
+
+    #     if jj == 100:
+    #         break
+
     # Just_Pole = image[y:y+h,x:x+w]
 
-def bbox_dict_to_list(l,t,w,h, image_size):
-#   h = bbox_dict.get('height')
-#   l = bbox_dict.get('left')
-#   t = bbox_dict.get('top')
-#   w = bbox_dict.get('width')
-
-  img_w, img_h = image_size
-
-  x1 = l/img_w
-  y1 = t/img_h
-  x2 = (l+w)/img_w
-  y2 = (t+h)/img_h
-  return [x1, y1, x2, y2]
+    for l in range(0,len(newLocation)):
+        image[newLocation[l]] = [42,42,165]
+    cv2.imshow("New Images",image)
+    # cv2.imwrite("newUpdated.jpg",image)
+    cv2.waitKey(0)
 
 if __name__=="__main__":
     main()
